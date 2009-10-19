270d1d654fc81a6549d825aabeecfd237203be68
vsemenov <lothix@gmail.com>
2009-10-19 11:01:07 -0500

    stats entry code

--- core/views/services/eventgoals.py
+++ core/views/services/eventgoals.py
@@ -2,9 +2,44 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
-from nihlapp.core.models import Event, EventType, Rink, Season, EventStatus
+from nihlapp.core.models import Event, Team, EventGoal
 from time import strptime, strftime
 from datetime import datetime
 
 @login_required
-def create(request): pass
+def create(request): 
+
+    # attempt to save a goal
+    try:
+        event = Event.objects.get(id = request.POST['event_id'])
+        teamFor = Team.objects.get(id = request.POST['goal_team'])
+        
+        # find out which team was scored against
+        if event.homeTeam == teamFor:
+            teamAgainst = event.awayTeam
+        else:
+            teamAgainst = event.homeTeam
+            
+        # populate goal object and call save
+        goal = EventGoal()
+        goal.event = event
+        goal.time = datetime(*strptime("%s:%s" % (request.POST['goal_time_minute'], request.POST['goal_time_second']), "%M:%S")[:6])
+        goal.team = teamFor
+        goal.againstTeam = teamAgainst
+        goal.period = request.POST['goal_period']
+        goal.player = request.POST['goal_player']
+        event.save()
+        
+        response = {'team': str(goal.team), 
+                    'player': goal.player, 
+                    'period': goal.period,
+                    'time_minute': strftime("%M", goal.time.timetuple()),
+                    'time_second': strftime("%S", goal.time.timetuple())}
+        
+    except Exception, error:
+        response = {'error': str(error)}
+    
+    #response = {'error': "got this: %s!" % (request.POST['event_am'])}
+    
+    json = simplejson.dumps(response)
+    return HttpResponse(json, mimetype = 'application/json')    
--- static/js/base.js
+++ static/js/base.js
@@ -22,14 +22,27 @@ function editTimeSlot() {
 
 //saves a goal record
 function saveGoal() {
-	var postVars = $('#goals :input:not(:checkbox), #goals :input:checkbox:checked').serialize();
+	if(!$('#goal_player').val()) {
+		alert("Please provide goal player number.");
+		return false;
+	}
+	if(!$('#goal_time_minute').val()) {
+		alert("Please provide goal minute.");
+		return false;
+	}
+	if(!$('#goal_time_second').val()) {
+		alert("Please provide goal second.");
+		return false;
+	}	
+	var postVars = $('#event_id, #goals :input:not(:checkbox), #goals :input:checkbox:checked').serialize();
 	$.post('/services/eventgoals/create', postVars,
 		function(response) {
 			if(response.error) {
-				$("#create_form").after("<tr><td colspan=6>Error occured while saving goal: " + response.error + "</td></tr>");
+				$("#goals").after("<tr><td colspan=6>Error occured while saving goal: " + response.error + "</td></tr>");
 			} else {
-				$("#create_form").after("<tr><td>Time Slot:</td><td>" + response.eventType + "</td><td>" +
-					response.rink + "</td><td colspan=2>" + response.date + "</td></tr>");
+				$("#goals").after("<tr><td>" + response.team + "</td><td>" +
+					response.player + "</td><td>" + response.period + 
+					"</td><td colspan=2>" + response.time_minute + ":" + response.time_second + "</td></tr>");
 			}
 		}, "json"
 	);
