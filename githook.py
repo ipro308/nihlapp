920c8ca4b39a3455ff4ba1d9050472bf64dae893
vsemenov <lothix@gmail.com>
2009-10-19 19:17:43 -0500

    updated test server hook

--- githook.py
+++ githook.py
@@ -5,16 +5,16 @@ from time import strftime
 def pull(request):
        
     try:
-        file = open("/home/vsemenov/logs/githook.log", 'a')
+        file = open("/home/vsemenov/hooks/githook.log", 'a')
         file.write("%s: Git pull request via %s\n" % (strftime("%Y-%m-%d %H:%M:%S"), request.META['REQUEST_METHOD']))
         file.close()
         os.system("cd %s && touch dummy.pyc && find . -type f -name '*.pyc' | xargs rm && git pull" % (os.path.dirname(os.path.realpath(__file__))))
-        os.system("touch /home/vsemenov/logs/nihlapp-restart")
+        os.system("touch /home/vsemenov/hooks/nihlapp-restart")
         status = "success"
         
     except Exception, error:
         status = str(error)
-        file = open("/home/vsemenov/logs/logs/githook.log", 'a')
+        file = open("/home/vsemenov/hooks/githook.log", 'a')
         file.write("%s: Git pull request ERROR: %s\n" % (strftime("%Y-%m-%d %H:%M:%S"), status))
         file.close()
     
