98168b3d1aafe5735d45de3b5840f64bf8c4bb42
Orbomac <Orbomac@dhcp57.westwn1.iit.edu>
2009-11-23 11:44:20 -0600

    added teams.py

--- core/views/teams.py
+++ core/views/teams.py
@@ -5,11 +5,11 @@ from nihlapp.core.models import Team, Event, EventStats
 def detail(request, team_id):
 
     # Look up the publisher (and raise a 404 if it can't be found).
-    try:
-        schedule_queryset = Event.objects.filter(homeTeam__id = team_id)
-        stats_queryset = EventStats.objects.filter(event__homeTeam__id = object_id )
-    except: 
-        raise Http404
+    #try:
+        #schedule_queryset = Event.objects.filter(homeTeam__id = team_id)
+        #stats_queryset = EventStats.objects.filter(event__homeTeam__id = object_id )
+    #except: 
+     #   raise Http404
 
     # Use the object_list view for the heavy lifting.
     return list_detail.object_detail(
@@ -18,5 +18,5 @@ def detail(request, team_id):
         template_name = "core/team_detail.html",
         template_object_name = "object",
         object_id = team_id,
-        extra_context = {"schedule_queryset" : schedule_queryset, "stats_queryset" : stats_queryset}
+        extra_context = {"schedule_queryset" : list(), "stats_queryset" : list()}
     )
