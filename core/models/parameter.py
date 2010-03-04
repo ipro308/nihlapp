f4cad328af9cee68aa351ba96c800653e9fd224f
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2010-03-04 00:38:41 -0600

    increased value sizes to increase compatability with PostgreSQL

--- core/models/parameter.py
+++ core/models/parameter.py
@@ -2,8 +2,8 @@ from django.db import models
 #from nihlapp.core.models import *
 
 class Parameter(models.Model):
-    name = models.CharField("Event Type Name", max_length = 30, unique = True)
-    value = models.CharField("Event Type Name", max_length = 30)
+    name = models.CharField("Event Type Name", max_length = 50, unique = True)
+    value = models.CharField("Event Type Name", max_length = 50)
 
     class Meta:
 		app_label = "core"
