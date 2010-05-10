dce4349688bf77db75e372e94c48d32696985a8e
Kevin Mutyaba <tiabasnk@gmail.com>
2010-05-10 10:51:19 -0500

    configured email settings to use gmail

--- settings.py
+++ settings.py
@@ -115,8 +115,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 #EMAIL SETTINGS CONFIGURATION
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tiabasnk@gmail.com'
-EMAIL_HOST_PASSWORD = '#li2baby'
+EMAIL_HOST_USER = 'kleo22@gmail.com'
+EMAIL_HOST_PASSWORD = '123kleo'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
